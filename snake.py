e3cc104ff8da2b8cb576661f5e63fef22e835b80
0sant1ago <santiagovaldivia2000@gmail.com>
2022-11-28 19:24:48 -0400

    first iteration

--- snake.py
+++ snake.py
@@ -3,21 +3,31 @@ from pygame.locals import *
 import time
 import random
 
-
 SIZE = 44
-BACK_ROUND = (164, 233, 118)
-TEXT_COL = (255, 255, 255)
+BACK_ROUND = (225, 145, 238)
+TEXT_COL = (225, 145, 238)
 
 
 class Snake:
     def __init__(self, back_round):
         self.screen = back_round
-        self.block = pygame.image.load("resources/block.jpg").convert()
+
+        arch = open('snake_type', 'r')
+        color_snake = arch.readline()
+        arch.close()
+        if color_snake[0] == 'g':
+            color_snake = "block"
+        if color_snake[0] == 'y':
+            color_snake = "block2"
+
+        self.block = pygame.image.load(str("resources/" + color_snake + ".jpg")).convert()
+
+        #self.block = pygame.image.load("resources/block.jpg").convert()
 
         self.direction = 'down'
         self.length = 1
-        self.x = [SIZE]*self.length
-        self.y = [SIZE]*self.length
+        self.x = [SIZE] * self.length
+        self.y = [SIZE] * self.length
 
     def move_right(self):
         self.direction = 'right'
@@ -39,9 +49,9 @@ class Snake:
         pygame.display.flip()
 
     def walk(self):
-        for i in range(self.length-1, 0, -1):
-            self.x[i] = self.x[i-1]
-            self.y[i] = self.y[i-1]
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
 
         if self.direction == 'left':
             self.x[0] -= SIZE
